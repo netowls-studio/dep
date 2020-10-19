5fe1796fbc05fc8bccc863bc5fc68618ffcd26f8
Wang Yucai <NetOwls-Studio@Outlook.com>
2020-10-19 09:51:08 +0800

    feat: 新增访问配置信息的接口 IConfigurationBroker。
    *******************************************************************************************************************************************************
    add: src/Dep.Common/Configuration/IConfigurationBroker.cs
         src/Dep.Common/Configuration/ConfigurationBroker.cs
    *******************************************************************************************************************************************************

---
+++ src/Dep.Common/Configuration/ConfigurationBroker.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * ********************************************************************************************
+ * Copyright © 2006 - 2020 Wang Yucai. All rights reserved.
+ * Licensed under the MS-PL License See LICENSE in the project root for license information.
+ *
+ *
+ * Author:             Wang Yucai
+ * Git Repository Url: https://github.com/netowls-studio/dep
+ * Created Time:       2020/10/19 9:46:30
+ * Code:               ConfigurationBroker.cs
+ * ********************************************************************************************
+ */
+
+namespace NetowlsStudio.Dep.Configuration
+{
+    /// <summary> 提供了访问 DEP 配置信息相关的基本方法。 </summary>
+    /// <typeparam name="TOptions"> 实现了 <see cref="IDepOptions" /> 接口的类型。 </typeparam>
+    /// <seealso cref="IConfigurationBroker{TOptions}" />
+    /// <seealso cref="IDepOptions" />
+    public abstract class ConfigurationBroker<TOptions> : IConfigurationBroker<TOptions>
+        where TOptions : class, IDepOptions
+    {
+        /// <summary> 获取配置信息。 </summary>
+        /// <returns> 实现了 <see cref="IDepOptions" /> 类型接口的对象实例。 </returns>
+        TOptions IConfigurationBroker<TOptions>.Get() => Get();
+
+        /// <summary> 获取配置信息。 </summary>
+        /// <returns> 实现了 <see cref="IDepOptions" /> 类型接口的对象实例。 </returns>
+        public abstract TOptions Get();
+    }
+}
