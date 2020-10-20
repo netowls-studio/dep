8e0fc4ab712b5ab2cd3adff99b8d9f580a4b6510
Wang Yucai <NetOwls-Studio@Outlook.com>
2020-10-20 13:33:49 +0800

    feat: 新增基于 .NET 诊断器记录运行时日志的方法 TraceListenerWriter。
    changes
    *******************************************************************************************************************************************************
    add: src/Dep.Runtime.Logging.Abstractions/TraceListenerWriter.cs
    modify: src/Dep.Runtime.Logging.Abstractions/LogWriter.cs
    *******************************************************************************************************************************************************

--- src/Dep.Runtime.Logging.Abstractions/TraceListenerWriter.cs
+++ src/Dep.Runtime.Logging.Abstractions/TraceListenerWriter.cs
@@ -6,38 +6,29 @@
  *
  * Author:             Wang Yucai
  * Git Repository Url: https://github.com/netowls-studio/dep
- * Created Time:       2020/10/20 13:17:39
- * Code:               LogWriter.cs
+ * Created Time:       2020/10/20 13:26:43
+ * Code:               TraceListenerWriter.cs
  * ********************************************************************************************
  */
 
 using System;
+using System.Diagnostics;
 
 namespace NetowlsStudio.Dep.Runtime.Logging
 {
     /// <summary>
-    /// 提供了记录运行时日志相关的方法。
+    /// 提供了基于 .NET <see cref="TraceListener" /> 记录运行时日志相关的方法。
     /// <para> 密闭的，不可以从此类型派生。 </para>
     /// </summary>
-    /// <seealso cref="ILogWriter" />
     /// <seealso cref="LogWriterBase" />
-    /// <seealso cref="LogWriterCollection" />
+    /// <seealso cref="TraceListener" />
     /// <remarks> 密闭的，不可以从此类型派生。 </remarks>
-    public sealed class LogWriter : LogWriterBase
+    public sealed class TraceListenerWriter : LogWriterBase
     {
-        /// <summary> 默认的日志服务。 </summary>
-        public static readonly ILogWriter CurrentWriter = new LogWriter();
-
-        /// <summary> 用于初始化一个 <see cref="LogWriter" /> 类型的对象实例。 </summary>
-        public LogWriter()
+        /// <summary> 用于初始化一个 <see cref="TraceListenerWriter" /> 类型的对象实例。 </summary>
+        public TraceListenerWriter()
         {
-            Writers = new LogWriterCollection(new ILogWriter[1] { new TraceListenerWriter() });
         }
-
-        /// <summary> 记录运行时日志的服务集合。 </summary>
-        /// <value> 获取 <see cref="LogWriterCollection" /> 类型的对象实例，用于表示记录运行时日志的服务集合。 </value>
-        /// <seealso cref="LogWriterCollection" />
-        public LogWriterCollection Writers { get; }
 
         /// <summary> 记录日志。 </summary>
         /// <param name="sourceType"> 记录日志的类型。 </param>
@@ -47,10 +38,10 @@ namespace NetowlsStudio.Dep.Runtime.Logging
         /// <seealso cref="LogEntryTypes" />
         protected override void InternalWrite(Type sourceType, string message, Exception exception = null, LogEntryTypes type = LogEntryTypes.Information)
         {
-            foreach (var writer in Writers)
-            {
-                writer.Write(sourceType, message, exception, type);
-            }
+            if (EqualsType(type, LogEntryTypes.Debug))
+                Debug.WriteLine(message);
+            else
+                Trace.WriteLine(message);
         }
     }
 }
