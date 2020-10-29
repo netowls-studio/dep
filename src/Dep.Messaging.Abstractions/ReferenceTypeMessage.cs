3064e805917906f779695a3b499f056a8bc44848
Wang Yucai <NetOwls-Studio@Outlook.com>
2020-10-29 09:49:45 +0800

    feat: 新增引用类型（类）的消息。

---
+++ src/Dep.Messaging.Abstractions/ReferenceTypeMessage.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * ********************************************************************************************
+ * Copyright © 2006 - 2020 Wang Yucai. All rights reserved.
+ * Licensed under the MS-PL License See LICENSE in the project root for license information.
+ *
+ *
+ * Author:             Wang Yucai
+ * Git Repository Url: https://github.com/netowls-studio/dep
+ * Created Time:       2020/10/29 9:45:30
+ * Code:               ReferenceTypeMessage.cs
+ * ********************************************************************************************
+ */
+
+using NetowlsStudio.Dep.Runtime.Serializations.Providers;
+using Newtonsoft.Json;
+using System;
+using System.Xml.Serialization;
+
+namespace NetowlsStudio.Dep.Messaging
+{
+    /// <summary> 提供了包含了引用类型数据消息相关的方法。 </summary>
+    /// <seealso cref="Message{TData}" />
+    [Serializable, XmlRoot(ElementName = XmlRootElementName, Namespace = XmlSerializerNamespaceDefaults.Uri)]
+    public class ReferenceTypeMessage<T> : Message<T>
+        where T : class, new()
+    {
+        /// <summary> 用于初始化一个 <see cref="ReferenceTypeMessage{T}" /> 类型的对象实例。 </summary>
+        public ReferenceTypeMessage()
+        {
+        }
+
+        /// <summary> 用于初始化一个 <see cref="ReferenceTypeMessage{T}" /> 类型的对象实例。 </summary>
+        /// <param name="messageData"> <typeparamref name="T" /> 类型的对象实例。 </param>
+        public ReferenceTypeMessage(T messageData) : base(messageData)
+        {
+        }
+
+        /// <summary> 消息数据。 </summary>
+        /// <value> 设置或获取 <typeparamref name="T" /> 类型的对象实例，用于表示消息数据。 </value>
+        [JsonProperty(MessageDataSerializePropertyName), XmlElement(ElementName = MessageDataSerializePropertyName, Namespace = XmlSerializerNamespaceDefaults.Uri)]
+        public override T MessageData { get => base.MessageData; set => base.MessageData = value; }
+    }
+}
