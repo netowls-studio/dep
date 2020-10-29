29d3087559cea5eaf3d071abfa5c73e6720deb17
Wang Yucai <NetOwls-Studio@Outlook.com>
2020-10-29 11:29:44 +0800

    feat: 为 IStreamSlicer 接口增加了异步方法。

--- src/Dep.Messaging.Abstractions/IO/IStreamSlicer.cs
+++ src/Dep.Messaging.Abstractions/IO/IStreamSlicer.cs
@@ -11,8 +11,10 @@
  * ********************************************************************************************
  */
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace NetowlsStudio.Dep.Messaging.IO
 {
@@ -23,6 +25,17 @@ namespace NetowlsStudio.Dep.Messaging.IO
         /// <param name="stream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
         /// <returns> <see cref="StreamFragmentInfo" /> 类型的对象实例集合。 </returns>
         /// <seealso cref="IEnumerable{T}" />
+        /// <seealso cref="StreamFragmentInfo" />
+        /// <exception cref="IOException"> </exception>
         IEnumerable<StreamFragmentInfo> Split(Stream stream);
+
+        /// <summary> (异步的方法) 分割流 <paramref name="stream" />。 </summary>
+        /// <param name="stream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
+        /// <returns> 包含了 <see cref="StreamFragmentInfo" /> 类型的对象实例集合和异常信息的 <see cref="Tuple{T1, T2}" />。 </returns>
+        /// <seealso cref="IEnumerable{T}" />
+        /// <seealso cref="Tuple{T1, T2}" />
+        /// <seealso cref="StreamFragmentInfo" />
+        /// <exception cref="IOException"> </exception>
+        Task<Tuple<IEnumerable<StreamFragmentInfo>, Exception>> SplitAsync(Stream stream);
     }
 }
