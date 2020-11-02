fc95d92061b70a06adbdb0118d48ff78b4ffb1c1
Wang Yucai <NetOwls-Studio@Outlook.com>
2020-11-02 10:24:10 +0800

    feat: 新增消息内容接口。

---
+++ src/Dep.Messaging.Abstractions/IMessageBody.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * ********************************************************************************************
+ * Copyright © 2006 - 2020 Wang Yucai. All rights reserved.
+ * Licensed under the MS-PL License See LICENSE in the project root for license information.
+ *
+ *
+ * Author:             Wang Yucai
+ * Git Repository Url: https://github.com/netowls-studio/dep
+ * Created Time:       2020/11/2 10:19:48
+ * Code:               IMessageBody.cs
+ * ********************************************************************************************
+ */
+
+namespace NetowlsStudio.Dep.Messaging
+{
+    /// <summary> 定义了消息内容的接口。 </summary>
+    public interface IMessageBody
+    {
+        /// <summary> 消息内容。 </summary>
+        /// <value> 设置或获取 <see cref="object" /> 类型的对象实例，用于表示消息内容。 </value>
+        object Content { get; set; }
+    }
+}
