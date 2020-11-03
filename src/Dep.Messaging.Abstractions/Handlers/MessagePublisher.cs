b761cfb7c7a85428b8fb33ffd2aa137dc71b623e
Wang Yucai <NetOwls-Studio@Outlook.com>
2020-11-03 10:54:45 +0800

    feat: 新增消息发布程序基类。

---
+++ src/Dep.Messaging.Abstractions/Handlers/MessagePublisher.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * ********************************************************************************************
+ * Copyright © 2006 - 2020 Wang Yucai. All rights reserved.
+ * Licensed under the MS-PL License See LICENSE in the project root for license information.
+ *
+ *
+ * Author:             Wang Yucai
+ * Git Repository Url: https://github.com/netowls-studio/dep
+ * Created Time:       2020/11/3 10:52:21
+ * Code:               MessagePublisher.cs
+ * ********************************************************************************************
+ */
+
+using System;
+using System.Threading.Tasks;
+
+namespace NetowlsStudio.Dep.Messaging.Handlers
+{
+    /// <summary> 提供了发布消息相关的基本方法。 </summary>
+    /// <seealso cref="IMessagePublisher" />
+    public abstract class MessagePublisher : IMessagePublisher
+    {
+        /// <summary> 用于初始化一个 <see cref="MessagePublisher" /> 类型的对象实例。 </summary>
+        protected MessagePublisher()
+        {
+        }
+
+        /// <summary> 发布消息。 </summary>
+        /// <param name="message"> 实现了 <see cref="IMessage" /> 类型接口的对象实例。 </param>
+        /// <seealso cref="IMessage" />
+        public abstract void Publish(IMessage message);
+
+        /// <summary> (异步的方法) 发布消息。 </summary>
+        /// <param name="message"> 实现了 <see cref="IMessage" /> 类型接口的对象实例。 </param>
+        /// <returns> 包含了可能的异常信息。 </returns>
+        /// <seealso cref="IMessage" />
+        /// <seealso cref="Task{TResult}" />
+        public virtual async Task<Exception> PublishAsync(IMessage message)
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    Publish(message);
+                    return null;
+                }
+                catch (Exception error)
+                {
+                    return error;
+                }
+            });
+        }
+    }
+}
